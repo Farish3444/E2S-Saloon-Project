2b0d9fc85d6ddc72db85e22e7796311016376780
ashpak.farish@awanoo.com <ashpak.farish@barunitsolutions.com>
2022-02-21 23:46:01 +0530

    footer betastage

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,6 +5,7 @@ import MailIcon from "@mui/icons-material/Mail";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import CallIcon from "@mui/icons-material/Call";
 
+
 const Footer = () => {
   return (
     <div className="footercontainer">
@@ -14,36 +15,31 @@ const Footer = () => {
         <div className="container">
           <div className="footer-cta pt-5 pb-5">
             <div className="row">
-              <div className="col-xl-4 col-md-4 mb-30">
-                <div className="single-cta">
-                  <MailIcon fontSize="large" style={{ color: "white" }} />
-                  <div className="cta-text">
-                    <h4>REACH-OUT US</h4>
-                    <span> KK NAGAR </span>
-                  </div>
+              <div className="single-cta">
+                <MailIcon fontSize="large" style={{ color: "white" }} />
+                <div className="cta-text">
+                  <h4>REACH-OUT US</h4>
+                  <span> KK NAGAR </span>
                 </div>
               </div>
 
-              <div className="col-xl-4 col-md-4 mb-30">
-                <div className="single-cta">
-                  <CallIcon fontSize="large" style={{ color: "white" }} />
-                  <div className="cta-text">
-                    <h4>Call and Book Your Appointment</h4>
-                    <span>98765432100</span>
-                  </div>
+              <div className="single-cta">
+                <CallIcon fontSize="large" style={{ color: "white" }} />
+                <div className="cta-text">
+                  <h4>Call and Book Your Appointment</h4>
+                  <span>98765432100</span>
                 </div>
               </div>
 
-              <div className="col-xl-4 col-md-4 mb-30">
-                <div className="single-cta">
-                  <MailIcon fontSize="large" style={{ color: "white" }} />
-                  <div className="cta-text">
-                    <h4>Mail us</h4>
-                    <span>mail@info.com</span>
-                  </div>
+              <div className="single-cta">
+                <MailIcon fontSize="large" style={{ color: "white" }} />
+                <div className="cta-text">
+                  <h4>Mail us</h4>
+                  <span>mail@info.com</span>
                 </div>
               </div>
             </div>
+
           </div>
         </div>
       </footer>
